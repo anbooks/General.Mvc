86f2ddea3c5ca9f545e6ba553bcd86ab330efbe1
Kevin <kevin_zhang2011@126.com>
2020-07-10 16:29:09 +0800

    #管理员Login登录页

--- General.Mvc/Areas/Admin/Controllers/LoginController.cs
+++ General.Mvc/Areas/Admin/Controllers/LoginController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using General.Entities;
 using General.Framework.Controllers.Admin;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,14 +11,29 @@ namespace General.Mvc.Areas.Admin.Controllers
     /// <summary>
     /// 后台管理登录控制器
     /// </summary>
-    [Route("admin/login")]
+    //[Route("admin/login")]
+    [Route("admin")]
     public class LoginController : AdminAreaController
     {
         //[Route("")]
-        [Route("",Name ="adminLogin")]
+        [Route("login", Name = "adminLogin")]
         public IActionResult LoginIndex()
         {
             return View();
         }
+
+        [HttpPost]
+        [Route("login")]
+        public IActionResult LoginIndex(LoginModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                AjaxData.Message = "请输入用户账号和密码";
+                return Json(AjaxData);
+            }
+            return Json(AjaxData);
+        }
+
+
     }
 }
