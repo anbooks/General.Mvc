6afea69d36613755197bb75d654ec8bc52208f96
Kevin <kevin_zhang2011@126.com>
2020-08-27 17:02:14 +0800

    #发布的时候需要添加的内容

--- General.Mvc/Program.cs
+++ General.Mvc/Program.cs
@@ -17,9 +17,17 @@ namespace General.Mvc
             BuildWebHost(args).Run();
         }
 
+        //public static IWebHost BuildWebHost(string[] args) =>
+        //    WebHost.CreateDefaultBuilder(args)
+        //        .UseStartup<Startup>()
+        //        .Build();
         public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+           WebHost.CreateDefaultBuilder(args)
+               .UseIISIntegration()
+               .UseKestrel()
+           .UseApplicationInsights()
+           .UseContentRoot(Directory.GetCurrentDirectory())
+               .UseStartup<Startup>()
+               .Build();
     }
 }
