5c1721ad17aa8897a9491f2d322bd065e9590e43
Kevin <kevin_zhang2011@126.com>
2020-09-24 11:22:42 +0800

    #kevin  20200924 页面框图及效果添加

---
+++ General.Mvc/Areas/Admin/Controllers/ITOrderImportController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using General.Framework.Controllers.Admin;
+using General.Framework.Menu;
+using Microsoft.AspNetCore.Mvc;
+
+namespace General.Mvc.Areas.Admin.Controllers
+{
+    //[Area("Admin")]
+    [Route("admin/itOrderImport")]
+    public class ITOrderImportController : AdminPermissionController
+    {
+        [Route("", Name = "itOrderImportIndex")]
+        [Function("订单数据导入", true, "menu-icon fa fa-caret-right", FatherResource = "General.Mvc.Areas.Admin.Controllers.ImportTransportationController", Sort = 1)]
+
+        public IActionResult ITOrderImportIndex()
+        {
+            return View();
+        }
+    }
+}
---
+++ General.Mvc/Areas/Admin/Controllers/ImportTransportationController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using General.Framework.Controllers.Admin;
+using General.Framework.Menu;
+using Microsoft.AspNetCore.Mvc;
+
+namespace General.Mvc.Areas.Admin.Controllers
+{
+    [Function("进口运输", true, "menu-icon fa fa-pencil-square-o")]
+    public class ImportTransportationController : AdminPermissionController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
