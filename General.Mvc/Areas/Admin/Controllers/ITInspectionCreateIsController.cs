ca58e529c033ff74af49b48be4994d15956583b3
Kevin <kevin_zhang2011@126.com>
2020-09-29 10:02:12 +0800

    #Kevin 首页滚动条

---
+++ General.Mvc/Areas/Admin/Controllers/ITInspectionCreateIsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using General.Framework.Controllers.Admin;
+using General.Framework.Menu;
+using Microsoft.AspNetCore.Mvc;
+
+namespace General.Mvc.Areas.Admin.Controllers
+{
+    [Route("admin/itInspectionCreateIs")]
+    public class ITInspectionCreateIsController : AdminPermissionController
+    {
+        [Route("", Name = "itInspectionCreateIs")]
+        [Function("生成送检单", true, "menu-icon fa fa-caret-right", FatherResource = "General.Mvc.Areas.Admin.Controllers.ImportTransportationController", Sort = 1)]
+
+        public IActionResult ITInspectionCreateIsIndex()
+        {
+            return View();
+        }
+    }
+}
--- General.Mvc/Areas/Admin/Controllers/ITSecondCheckIsController.cs
+++ General.Mvc/Areas/Admin/Controllers/ITSecondCheckIsController.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using General.Framework.Controllers.Admin;
 using General.Framework.Menu;
 using Microsoft.AspNetCore.Mvc;
 
 namespace General.Mvc.Areas.Admin.Controllers
 {
     [Route("admin/itSecondCheckIs")]
-    public class ITSecondCheckIsController : Controller
+    public class ITSecondCheckIsController : AdminPermissionController
     {
         [Route("", Name = "itSecondCheckIs")]
         [Function("是否需要二检", true, "menu-icon fa fa-caret-right", FatherResource = "General.Mvc.Areas.Admin.Controllers.ImportTransportationController", Sort = 1)]
