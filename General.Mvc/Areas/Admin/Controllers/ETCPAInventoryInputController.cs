0d035255f8730ef04b1a550a121a6947ad924a08
Kevin <kevin_zhang2011@126.com>
2020-09-29 14:41:28 +0800

    #Kevin 出口页面

---
+++ General.Mvc/Areas/Admin/Controllers/ETCPAInventoryInputController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using General.Framework.Menu;
+using Microsoft.AspNetCore.Mvc;
+
+namespace General.Mvc.Areas.Admin.Controllers
+{
+    [Route("admin/etCPAInventoryInput")]
+    public class ETCPAInventoryInputController : Controller
+    {
+        [Route("", Name = "etCPAInventoryInput")]
+        [Function("综保区填写核注清单", true, "menu-icon fa fa-caret-right", FatherResource = "General.Mvc.Areas.Admin.Controllers.ExportTransportationController", Sort = 1)]
+
+        public IActionResult ETCPAInventoryInputIndex()
+        {
+            return View();
+        }
+    }
+}
