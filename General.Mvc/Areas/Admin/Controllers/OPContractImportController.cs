f7495661caf3bda19c430fa041970293506a312b
Kevin <kevin_zhang2011@126.com>
2020-09-29 13:01:46 +0800

    #外部加工主要页面

---
+++ General.Mvc/Areas/Admin/Controllers/OPContractImportController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using General.Framework.Controllers.Admin;
+using General.Framework.Menu;
+using Microsoft.AspNetCore.Mvc;
+
+namespace General.Mvc.Areas.Admin.Controllers
+{
+    [Route("admin/opContractImport")]
+    public class OPContractImportController : AdminPermissionController
+    {
+
+        [Route("", Name = "opContractImport")]
+        [Function("合同导入", true, "menu-icon fa fa-caret-right", FatherResource = "General.Mvc.Areas.Admin.Controllers.OutwardProcessingController", Sort = 1)]
+
+        public IActionResult OPContractImportIndex()
+        {
+            return View();
+        }
+    }
+}
