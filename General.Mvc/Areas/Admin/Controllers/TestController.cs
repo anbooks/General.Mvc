0b62e2caa53fa923027f186db83c83e6d860da62
Kevin <kevin_zhang2011@126.com>
2020-10-29 15:34:35 +0800

    #附件上传V1.0

--- General.Mvc/Areas/Admin/Controllers/TestController.cs
+++ General.Mvc/Areas/Admin/Controllers/TestController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using General.Entities;
@@ -8,7 +9,9 @@ using General.Framework.Controllers.Admin;
 using General.Framework.Datatable;
 using General.Framework.Menu;
 using General.Services.test_JqGrid;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace General.Mvc.Areas.Admin.Controllers
 {
@@ -171,6 +174,44 @@ namespace General.Mvc.Areas.Admin.Controllers
             return Json(pro);
         }
 
+        [Route("testUploadImage", Name = "testUploadImage")]
+        [Function("测试上传图片", false, FatherResource = "General.Mvc.Areas.Admin.Controllers.TestIndex")]
+        public IActionResult TestUploadImage()
+        {
+            return View();
+        }
 
+        [HttpPost]
+        [Route("registerResult", Name = "registerResult")]
+        public string RegisterResult([FromForm(Name = "avatar")]List<IFormFile>  files)
+        {
+
+             //var files = Request.Form.Files;
+            //string test= Request.Form["phone"].ToString();
+            files.ForEach(file =>
+            {
+                var fileName = file.FileName;
+                string fileExtension = file.FileName.Substring(file.FileName.LastIndexOf(".") + 1);//获取文件名称后缀 
+                //保存文件
+                var stream = file.OpenReadStream();
+                // 把 Stream 转换成 byte[] 
+                byte[] bytes = new byte[stream.Length];
+                stream.Read(bytes, 0, bytes.Length);
+                // 设置当前流的位置为流的开始 
+                stream.Seek(0, SeekOrigin.Begin);
+                // 把 byte[] 写入文件 
+                FileStream fs = new FileStream("D:\\" + file.FileName, FileMode.Create);
+                BinaryWriter bw = new BinaryWriter(fs);
+                bw.Write(bytes);
+                bw.Close();
+                fs.Close();
+            });
+            return "OK";
+
+           
+        }  
+
+
+
+        }
     }
-}
